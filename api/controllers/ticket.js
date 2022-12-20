4a4c70be4393cdcf473d6adea56eb8d48416361a
Simonas Lukoševičius <simonaslukosevicius@Simonass-MacBook-Pro.local>
2022-12-20 19:55:14 +0200

    added /buyTicket endpoint

--- api/controllers/ticket.js
+++ api/controllers/ticket.js
@@ -1,4 +1,6 @@
 const TicketSchema = require("../models/ticketMod");
+const UserSchema = require("../models/userMod");
+const jwt = require("jsonwebtoken");
 
 module.exports.CREATE_TICKET = (req, res) => {
   const ticket = new TicketSchema({
@@ -11,4 +13,25 @@ module.exports.CREATE_TICKET = (req, res) => {
   ticket.save().then(() => {
     return res.status(200).json({ Message: "Ticket saved." });
   });
+};
+
+module.exports.BUY_TICKET = async (req, res) => {
+  const userId = req.body.user_id;
+  const ticketId = req.body.ticket_id;
+
+  const user = await UserSchema.findById(userId);
+  const ticket = await TicketSchema.findById(ticketId);
+
+  if (user.money_balance < ticket.ticket_price) {
+    res.status(400).json({ Message: "Insufficient funds." });
+  } else {
+    const newBalance = user.money_balance - ticket.ticket_price;
+
+    UserSchema.updateOne(
+      { _id: userId },
+      { money_balance: newBalance, $push: { bought_tickets: ticketId } }
+    ).then(() => {
+      return res.status(200).json({ Message: "Ticket was bought." });
+    });
+  }
 };
